10c666c021c66a283b75ae4efb12038f02522490
Trishan Wagle <mailtotrishan@gmail.com>
2025-12-04 16:31:37 +0545

    feat: navigate to login from splash after a timeout

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:kaarya/screens/login_screen.dart';
 import 'package:kaarya/widgets/app_logo_widget.dart';
 import 'package:kaarya/widgets/loader_widget.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void _navigateToLoginScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLoginScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
