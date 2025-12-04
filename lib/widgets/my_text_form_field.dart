3d9b475d0b6f7b087b53bc8be399a982e269da73
Trishan Wagle <mailtotrishan@gmail.com>
2025-12-04 17:47:48 +0545

    feat: custom text form field widget

---
+++ lib/widgets/my_text_form_field.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class MyTextFormField extends StatefulWidget {
+  const MyTextFormField({
+    super.key,
+    required this.controller,
+    required this.text,
+    this.inputType = TextInputType.text,
+    this.validationErrorMessage = "Please enter some value",
+    this.prefixIcon,
+    this.obscureText = false,
+    this.onChanged,
+    this.validator,
+  });
+
+  final TextEditingController controller;
+  final String text;
+  final TextInputType inputType;
+  final String validationErrorMessage;
+
+  final Widget? prefixIcon;
+  final bool obscureText;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  @override
+  State<MyTextFormField> createState() => _MyTextFormFieldState();
+}
+
+class _MyTextFormFieldState extends State<MyTextFormField> {
+  bool _isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.inputType,
+      obscureText: widget.obscureText ? _isObscured : false,
+      onChanged: widget.onChanged,
+
+      validator:
+          widget.validator ??
+          (value) {
+            if (value == null || value.isEmpty) {
+              return widget.validationErrorMessage;
+            }
+            return null;
+          },
+
+      decoration: InputDecoration(
+        hintText: widget.text,
+
+        prefixIcon: widget.prefixIcon,
+
+        suffixIcon: widget.obscureText
+            ? IconButton(
+                icon: Icon(
+                  _isObscured ? Icons.visibility_off : Icons.visibility,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isObscured = !_isObscured;
+                  });
+                },
+              )
+            : null,
+
+        border: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Color.fromRGBO(208, 210, 212, 1)),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color.fromRGBO(4, 113, 182, 1.0),
+            width: 1.5,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
