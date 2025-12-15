8441d6aebb46ce9cabcca295fbe4c69832cc914d
Trishan Wagle <mailtotrishan@gmail.com>
2025-12-15 20:54:03 +0545

    feat: top border on active bottom nav

--- lib/screens/bottom_navigation_screen.dart
+++ lib/screens/bottom_navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kaarya/screens/bottom_screen/overview_screen.dart';
+import 'package:kaarya/theme/app_colors.dart';
 
 class BottomNavigationScreen extends StatefulWidget {
   const BottomNavigationScreen({super.key});
@@ -22,48 +23,63 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: lstBottomScreens[_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(color: Colors.grey.shade300, width: 1),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 1,
+            color: Colors.grey.shade300,
+            child: Row(
+              children: List.generate(5, (index) {
+                return Expanded(
+                  child: Container(
+                    height: 3,
+                    color: _selectedIndex == index
+                        ? AppColors.primary
+                        : Colors.transparent,
+                  ),
+                );
+              }),
+            ),
           ),
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.dashboard_rounded),
-              icon: Icon(Icons.dashboard_outlined),
-              label: "Overview",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.explore),
-              icon: Icon(Icons.explore_outlined),
-              label: "Explore",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.work),
-              icon: Icon(Icons.work_outline_outlined),
-              label: "InterviewAI",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.leaderboard),
-              icon: Icon(Icons.leaderboard_outlined),
-              label: "Leaderboard",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(Icons.document_scanner),
-              icon: Icon(Icons.document_scanner_outlined),
-              label: "ResumeAI",
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-        ),
+
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.dashboard_rounded),
+                icon: Icon(Icons.dashboard_outlined),
+                label: "Overview",
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.explore),
+                icon: Icon(Icons.explore_outlined),
+                label: "Explore",
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.work),
+                icon: Icon(Icons.work_outline_outlined),
+                label: "InterviewAI",
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.leaderboard),
+                icon: Icon(Icons.leaderboard_outlined),
+                label: "Leaderboard",
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.document_scanner),
+                icon: Icon(Icons.document_scanner_outlined),
+                label: "ResumeAI",
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
