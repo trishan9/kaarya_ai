d93e8f223db071ac2464c6f405b68a0c4da9046c
Trishan Wagle <mailtotrishan@gmail.com>
2025-12-31 09:05:22 +0545

    feat: auth entity and auth repository in domain layer.

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:kaarya/core/error/failures.dart';
+import 'package:kaarya/features/auth/domain/entities/auth_entity.dart';
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, bool>> registerUser(AuthEntity user);
+  Future<Either<Failure, AuthEntity>> loginUser(String email, String password);
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+  Future<Either<Failure, bool>> logoutUser();
+}
